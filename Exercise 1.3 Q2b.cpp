9879eca6552111d58fee6b87c109dc86475f9b68
Yoong Cheng Nee <55399557+krystal826@users.noreply.github.com>
2019-11-17 12:34:47 +0800

    Create Exercise 1.3 Q2b.cpp

---
+++ Exercise 1.3 Q2b.cpp
@@ -0,0 +1,20 @@
+// Exercise 1.3 Q2b: numbers (graph plotting)
+//Author: Yoong Cheng Nee
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "10\n";
+	cout << "9\n";
+	cout << "8\n";
+	cout << "7           x\n";
+	cout << "6 x\n";
+	cout << "5                    x\n";
+	cout << "4   x\n";
+	cout << "3               x\n";
+	cout << "2     x\n";
+	cout << "1 x\n";
+	cout << "0 1 2 3 4 5 6 7 8 9 10\n\n\n";
+}
