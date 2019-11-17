b1bf1f5649abe1cb1dadf1863a21ac472fe94cf2
Yoong Cheng Nee <55399557+krystal826@users.noreply.github.com>
2019-11-17 12:35:12 +0800

    Create Exercise 1.3 Q2a.cpp

---
+++ Exercise 1.3 Q2a.cpp
@@ -0,0 +1,12 @@
+// Exercise 1.3 Q2a : UTEM
+//Author: Yoong Cheng Nee
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "*******************************************\n";
+	cout << "*******\t\tUTEM\t\t***********\n";
+	cout << "*******************************************\n";
+}
