973ecabc47bf35205b76a07a2927b5d6f855afd8
Yoong Cheng Nee <55399557+krystal826@users.noreply.github.com>
2019-11-17 12:35:53 +0800

    Create Exercise 1.3 Q1.cpp

---
+++ Exercise 1.3 Q1.cpp
@@ -0,0 +1,14 @@
+// Exercise 1.3 Q1 : Intro about myself
+//Author: Yoong Cheng Nee
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "University :  Universiti Teknikal Malaysia, Melaka\n";
+	cout << "Faculty :  FTMK\n";
+	cout << "Programme :  BITI\n\n\n";
+	cout << "My name is Yoong Cheng Nee. I am from Melaka.\n";
+}
+© 2019 GitHub, Inc.
